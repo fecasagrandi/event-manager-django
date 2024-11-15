70fbce5a86db639675bf01da0f91ab88a7b09787
Felipe Moreira Casagrandi <157093522+fecasagrandi@users.noreply.github.com>
2024-11-15 16:11:31 -0300

    add initial models and admin registration for Evento, Participante, Ingresso, Palestrante, and Feedback

--- gestao_eventos/core/admin.py
+++ gestao_eventos/core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Evento, Participante, Ingresso, Palestrante, Feedback
 
-# Register your models here.
+admin.site.register(Evento)
+admin.site.register(Participante)
+admin.site.register(Ingresso)
+admin.site.register(Palestrante)
+admin.site.register(Feedback)
